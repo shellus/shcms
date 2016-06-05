a71f523e2b7b6747086ddfdd77982426d55c1c3a
shellus <shellus@endaosi.com>
2016-06-05 11:01:28 +0800

    修改ModelHelper支持静态调用。

--- app/ModelTrait/ModelHelperTrait.php
+++ app/ModelTrait/ModelHelperTrait.php
@@ -8,29 +8,30 @@
 
 namespace App\ModelTrait;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
 use Request;
 trait ModelHelperTrait
 {
 
-    public function scopeAutoWithAll (Builder $builder) {
-        $this -> scopeAutoTimeScope($builder);
-        $this -> scopeAutoLimitScope($builder);
-        $this -> scopeAutoOrderScope($builder);
-        $this -> scopeAutoEqualFields($builder, Request::all());
+    public static function scopeAutoWithAll (Builder $builder) {
+        static::scopeAutoTimeScope($builder);
+        static::scopeAutoLimitScope($builder);
+        static::scopeAutoOrderScope($builder);
+        static::scopeAutoEqualFields($builder, Request::all());
     }
 
-    public function scopeAutoTimeScope(Builder $builder){
+    public static function scopeAutoTimeScope(Builder $builder){
         Request::input('start_at') !== null &&
         $builder->where('created_at', '>', Request::input('start_at'));
         Request::input('stop_at') !== null &&
         $builder->where('created_at', '<', Request::input('stop_at'));
     }
 
-    public function scopeAutoLimitScope(Builder $builder){
+    public static function scopeAutoLimitScope(Builder $builder){
         Request::input('limit')!== null && $builder->take(Request::input('limit'));
     }
 
-    public function scopeAutoOrderScope(Builder $builder){
+    public static function scopeAutoOrderScope(Builder $builder){
         if(Request::input('order') !== null){
             if(Request::input('order') === "rand"){
                 $builder->orderByRaw("RAND()");
@@ -40,8 +41,8 @@ trait ModelHelperTrait
         }
     }
 
-    public function scopeAutoEqualFields(Builder $builder, $fields){
-        $columns = \Schema::getColumnListing($this -> getTable());
+    public static function scopeAutoEqualFields(Builder $builder, $fields){
+        $columns = \Schema::getColumnListing((new static) -> getTable());
 
         foreach ($fields as $field => $value) {
             if(Request::input($field) !== null && in_array($field, $columns)){
