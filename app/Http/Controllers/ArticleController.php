aab6c1c996e1671426fe0966acc86a670b7f67fc
shellus <shellus@endaosi.com>
2016-06-15 16:26:33 +0800

    添加控制器方法

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,8 @@ use App\Article;
 use App\Http\Controllers\ControllerTrait\RestControllerTrait;
 
 use App\Http\Requests;
+use App\Tag;
+use App\User;
 
 class ArticleController extends Controller
 {
@@ -14,5 +16,31 @@ class ArticleController extends Controller
     public function __construct()
     {
         $this -> model = new Article();
+    }
+    public function index($slug_or_id){
+        return view('article.list',[
+            'list_type' => 'default',
+            'title' => '内容列表',
+            'model' => $this -> model -> paginate()
+        ]);
+    }
+    public function show($id)
+    {
+        $data = $this -> model -> with('user') -> findOrFail($id);
+
+        return view('article/show', ['model' => $data]);
+    }
+    public function getTagAdd($id,$tag_id){
+
+        $this -> model -> findOrFail($id) -> tags() -> attach($tag_id);
+        return back();
+    }
+    public function getTag($slug_or_id){
+        $tag = Tag::where(is_numeric($slug_or_id)?'id':'slug',$slug_or_id) -> firstOrFail();
+        return view('article.list',[
+            'list_type' => 'tag',
+            'title' => $tag -> title,
+            'model' => $tag -> articles() -> paginate()
+        ]);
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,4 +15,19 @@ class UserController extends Controller
     {
         $this -> model = new User();
     }
+
+    public function updateAvatar(Request $request, \Storage $storage)
+    {
+        $user_id = $request['user_id'];
+        $user = User::findOrFail($user_id);
+
+        dump($user);
+        dd($request->file('avatar') -> getBasename());
+        $storage -> disk('public')->put('',file_get_contents($request->file('avatar')->getRealPath()));
+    }
+    public function edit($id)
+    {
+        $data = $this -> model -> findOrFail($id);
+        return view('user/edit', ['model' => $data, 'submit_url' => route('avatar.store')]);
+    }
 }
