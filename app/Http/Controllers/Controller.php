910fc8c8ac39cb3725b96fb9b277a8efe2aea5cf
shellus <shellus@endaosi.com>
2016-06-15 16:27:17 +0800

    添加基础类成员。解除IDE警告

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,5 +11,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
 class Controller extends BaseController
 {
+    /** @var  Model */
+    protected $model;
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 }
