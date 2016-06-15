643c8cfb4b0ad42e63d0d06d72025f3b4370b9f7
shellus <shellus@endaosi.com>
2016-06-15 16:20:39 +0800

    添加路由注释

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,7 +9,13 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+// --------------------- System Routes ---------------------
 
+// Index Routes
+Route::get('/', ['as' => 'index','uses' => 'IndexController@getIndex']);
+Route::get('/test', ['as' => 'test','uses' => 'IndexController@getTest']);
+
+// Auth Routes...
 Route::get('login', ['as' => 'get_login', 'uses' => 'Auth\AuthController@showLoginForm']);
 Route::post('login', ['as' => 'post_login', 'uses' => 'Auth\AuthController@login']);
 Route::get('logout', ['as' => 'get_logout', 'uses' => 'Auth\AuthController@logout']);
@@ -23,14 +29,28 @@ Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
 Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
 Route::post('password/reset', 'Auth\PasswordController@reset');
 
-Route::group(['middleware' => ['auth','permission:access_site']], function () {
+// permission require
+//Route::group(['middleware' => ['auth','permission:access_site']], function () {
+//
+//});
 
-});
-Route::get('/', ['as' => 'index','uses' => 'IndexController@getIndex']);
-Route::get('/test', ['as' => 'test','uses' => 'IndexController@getTest']);
+// User Home Routes...
+Route::post('user/avatar', [
+    'as' => 'avatar.store',
+    'uses' => 'UserController@updateAvatar',
+    'middleware' => 'auth',
+]);
 
-Route::resource('/user', 'UserController');
+// --------------------- Data Model Routes ---------------------
+
+// Article Routes...
+
 Route::resource('/article', 'ArticleController');
+Route::get('/article/{slug_or_id}', ['as' => 'article.index','uses' => 'ArticleController@index']);
+
+Route::get('/article/{article_id}/{tag_id}', ['as' => 'article.tag.add','uses' => 'ArticleController@getTagAdd']);
+Route::get('/tag/{slug_or_id}', ['as' => 'article.tag.index','uses' => 'ArticleController@getTag']);
+
 Route::resource('/tag', 'TagController');
 Route::resource('/category', 'CategoryController');
 Route::resource('/file', 'FileController');
