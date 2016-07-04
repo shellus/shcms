99876d8ce530954b634c14b8fdc5c4f973a25c2d
shellus <shellus@endaosi.com>
2016-07-04 23:53:58 +0800

    修复·评论·命令错误

--- database/migrations/2016_05_20_054326_create_commits_table.php
+++ database/migrations/2016_05_20_054326_create_commits_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCommentsTable extends Migration
+class CreateCommitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('commits', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('article_id') -> unsigned();
             $table->integer('user_id') -> unsigned();
@@ -30,6 +30,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('comments');
+        Schema::drop('commits');
     }
 }
