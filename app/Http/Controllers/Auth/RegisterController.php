025f660b2c440a277da3cf0eb6211789102dc1d8
shellus <shellus@endaosi.com>
2016-10-13 17:03:44 +0800

    修复seed user时的一个错误

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -60,7 +60,7 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return User
      */
-    protected function create(array $data)
+    public function create(array $data)
     {
         return User::create([
             'name' => $data['name'],
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,12 +13,11 @@ class UserSeeder extends Seeder
     public function run()
     {
         $users_data = [
-            ['name' => '娃娃脾气', 'email' => 'shellus@endaosi.com','password' => bcrypt('a7245810')],
+            ['name' => '娃娃脾气', 'email' => 'shellus@endaosi.com','password' => 'a7245810'],
         ];
 
         foreach($users_data as $user_data){
-            $user = User::create($user_data);
-            $user -> save();
+            $user = with(new \App\Http\Controllers\Auth\RegisterController()) -> create($user_data);
             $this -> command -> info('insert user: ' . $user['email']);
         }
 
