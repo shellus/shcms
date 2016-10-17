72f4b41b86c0b3ad53547bf3831d330e23909d41
shellus <shellus@endaosi.com>
2016-10-17 22:44:32 +0800

    debug模式下输出sql日志

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\ServiceProvider;
-
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,8 +15,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $log_file = storage_path('logs'.DIRECTORY_SEPARATOR.'sql.log');
+        $sqlLogger = new Logger('logic', [new StreamHandler($log_file)]);
+
         if (env('APP_DEBUG')){
-            \DB::enableQueryLog();
+            \DB::listen(function(QueryExecuted $event)use($sqlLogger) {
+                $sql = $event -> sql;
+                $bindings = $event -> bindings;
+                $time = $event -> time;
+
+                foreach ($bindings as $binding){
+                    $sql = preg_replace ('/\?/i', '\'' . $binding . '\'', $sql, 1);
+                }
+                $log_str = 'sql:' . $sql . " | "
+                    . 'time:' . $time . 'ms';
+                $sqlLogger -> info($log_str);
+            });
         }
 
 
