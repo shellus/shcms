89ea6935e7bdec1dd3ac0266e64f15c039271804
shellus <shellus@endaosi.com>
2016-10-17 23:07:59 +0800

    优化sql日志格式

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -16,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $log_file = storage_path('logs'.DIRECTORY_SEPARATOR.'sql.log');
-        $sqlLogger = new Logger('logic', [new StreamHandler($log_file)]);
+        $sqlLogger = new Logger('sql', [new StreamHandler($log_file)]);
 
         if (env('APP_DEBUG')){
             \DB::listen(function(QueryExecuted $event)use($sqlLogger) {
@@ -27,9 +28,14 @@ class AppServiceProvider extends ServiceProvider
                 foreach ($bindings as $binding){
                     $sql = preg_replace ('/\?/i', '\'' . $binding . '\'', $sql, 1);
                 }
-                $log_str = 'sql:' . $sql . " | "
-                    . 'time:' . $time . 'ms';
-                $sqlLogger -> info($log_str);
+                $context = [
+                    'time' => $time,
+                ];
+                if(\Auth::check()){
+                    $context['user_id'] = \Auth::user() -> id;
+                    $context['user_name'] = \Auth::user() -> name;
+                }
+                $sqlLogger -> info($sql, $context);
             });
         }
 
