064fc4a106e03dbdb9c05a4c2941ba6b6cb7cdba
shellus <shellus@endaosi.com>
2016-12-21 16:07:12 +0800

    优化头像上传后提示信息

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,10 +11,13 @@ class UserController extends Controller
     {
         $user = \Auth::user();
         $file = $request->file('avatar');
+        if(!$file->isValid()){
+            return $this->fail('上传失败' . $file -> getErrorMessage());
+        }
         $save_path = 'avatar/' . \Auth::user() -> id;
         $file_model = File::createFormUploadFile($file, $save_path);
         $user -> avatar() ->associate($file_model);
         $user -> save();
-        return '头像上传成功';
+        return $this->success('头像上传成功');
     }
 }
