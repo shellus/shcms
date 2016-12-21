fdcb384424fa3643594ffbd4d5ab1590e06b4647
shellus <shellus@endaosi.com>
2016-12-21 16:34:56 +0800

    优化个人中心显示

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,16 +18,16 @@
                     <div class="panel-heading">个人信息</div>
 
                     <div class="panel-body">
+                        <span><b>{{ \Auth::user() -> name }}</b></span>
 
                         <form action="/user/update-avatar" method="POST" enctype="multipart/form-data">
                             {{ csrf_field() }}
                             <span>头像: </span>
-                            <span><img width="100" height="100" src="{{ \Auth::user() -> avatarUrl }}"></span>
+                            <span><img style="padding: 15px;" class="img-circle" width="100" height="100" src="{{ \Auth::user() -> avatarUrl }}"></span>
                             <input name="avatar" type="file" value="选择文件">
                             <input type="submit" value="上传">
                         </form>
-                        <span>用户名: </span>
-                        <span>{{ \Auth::user() -> name }}</span>
+
                     </div>
                 </div>
 
