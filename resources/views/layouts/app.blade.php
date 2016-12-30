468dd8ae4cc7ed5df46e42c8daafa85c8f709d6a
shellus <shellus@endaosi.com>
2016-12-30 15:46:20 +0800

    优化语言本地化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,8 +90,8 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        <li><a href="{{ url('/login') }}">登录</a></li>
+                        <li><a href="{{ url('/register') }}">注册</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -101,7 +101,7 @@
                             <ul class="dropdown-menu" role="menu">
                                 <li>
                                     <a href="{{ url('/home') }}" >
-                                        Home
+                                        用户中心
                                     </a>
                                 </li>
 
@@ -109,7 +109,7 @@
                                     <a href="{{ url('/logout') }}"
                                         onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                        Logout
+                                        注销
                                     </a>
 
                                     <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
