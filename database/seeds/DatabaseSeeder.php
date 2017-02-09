03dd575152906aa55a03be5603c992b3bbb661e3
shellus <shellus@endaosi.com>
2017-02-09 18:36:02 +0800

    redis检查

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // 测试redis是否就绪
+        try{
+            \PRedis::get('test');
+        }catch (\Predis\Connection\ConnectionException $e){
+            $this->command->error('Unable to connect to redis ' . config('database.redis.default.host') . ':'
+                . config('database.redis.default.port'));
+            throw $e;
+        }
+
         $this->call(PermissionsSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(ArticlesSeeder::class);
