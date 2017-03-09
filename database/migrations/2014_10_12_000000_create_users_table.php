f101628854b50b709c3d709348003e2f2ad1bfac
shellus <shellus@endaosi.com>
2017-03-09 18:18:05 +0800

    取消用户名数据库唯一索引

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->integer('avatar_id') -> nullable() -> unsigned();
